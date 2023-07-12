690b457daa13c6f686e7c44d356004fe67968a2e
Artsiom Miachyn <artsiom.miachyn@intexsoft.by>
2023-07-13 01:12:06 +0300

    First commit

---
+++ src/main/java/com/example/demo/PrepareToBattle.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class PrepareToBattle implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        int warriors = (int) delegateExecution.getVariable("warriors");
+        int enemyWarriors = (int) (Math.random() * 100);
+        String battleStatus = "Undefiend";
+        boolean isWin = false;
+
+        if (warriors < 1 || warriors>100){
+            throw new BpmnError("warriorsError");
+        }
+        if ((warriors - enemyWarriors) > 0) {
+            isWin = true;
+            battleStatus="Victory";
+        }else {
+            battleStatus = "Fail! :(";
+        }
+        delegateExecution.setVariable("enemyWarriors", enemyWarriors);
+        delegateExecution.setVariable("battleStatus", battleStatus);
+        delegateExecution.setVariable("isWin",isWin);
+    }
+}
